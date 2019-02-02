34c09cff3bfd1cfe73432c5b11b8594846c0a68b
benarousfarouk <farouk.benarous26@gmail.com>
2019-02-02 04:13:01 +0000

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,54 @@
+# Heart Desease  Diagnostics using neural network
+
+
+## Overview
+
+activation function : Sigmoid 
+
+Optimazer  : RMS Optimazer based on gradient decent
+
+number of inputs  :12 wich are the features of the cells  : 
+age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal
+
+number of hidden layers : 7 in the best cases 
+
+number of output : 1 because its an binary clasification (Malignant begningn)
+
+##### features : 
+Attribute Information: 
+> 1. age 
+> 2. sex 
+> 3. chest pain type (4 values) 
+> 4. resting blood pressure 
+> 5. serum cholestoral in mg/dl 
+> 6. fasting blood sugar > 120 mg/dl
+> 7. resting electrocardiographic results (values 0,1,2)
+> 8. maximum heart rate achieved 
+> 9. exercise induced angina 
+> 10. oldpeak = ST depression induced by exercise relative to rest 
+> 11. the slope of the peak exercise ST segment 
+> 12. number of major vessels (0-3) colored by flourosopy 
+> 13. thal: 3 = normal; 6 = fixed defect; 7 = reversable defect
+##### lable :
+Target 1 or 0  binary classification 
+ 
+
+##### how  the prediction works ?
+So first to to train my model I have too a dataset bunch of examples of Cells whather its benign of malignant and then 
+train my model with it after I finished the training you get the Best optimal  parameters  to make the predictions 
+
+
+## Here are some helpful links:
+###### Dataset details to understand how it had been collected 
+https://www.kaggle.com/ronitf/heart-disease-uci
+
+## Framework
+Tensorflow 
+
+## Usage
+
+Just run ``python3 demo.py`` to see the results:
+
+To visulaze your Graph in tensorboard just run ``tensorboard --logdir=/tmp/mnist_tutorial/``
+wich the path depends on what you have set in the code "Logdir"
+
---
+++ demo.py
@@ -0,0 +1,106 @@
+import pandas as pd  # A beautiful library to help us work with data as tables
+import tensorflow as tf  # Fire from the gods
+
+if __name__ == "__main__":
+
+    # for the winwods users you should change this path
+    LOGDIR = "/tmp/mnist_tutorial/"
+
+    # Let's have Pandas load our dataset as a dataframe
+    dataframe = pd.read_csv("datasetcsv.csv")
+    # remove columns we don't care about
+
+    inputX = dataframe.loc[:,
+             ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca',
+              'thal']].values
+
+    inputY = dataframe.loc[:, ["target"]].values
+
+    # Let's prepare some parameters for the training process
+
+    # Parameters
+    n_input = 13  # features
+    n_hidden = 7  # hidden nodes
+    n_output = 1  # lables
+    learning_rate = 0.001
+    training_epochs = 1000000  # simply iterations
+    display_step = 10000  # to split the display
+    n_samples = inputY.size  # number of the instances
+
+    tf.reset_default_graph()
+    sess = tf.Session()
+
+    X = tf.placeholder(tf.float32, name="X")
+    tf.summary.histogram("inputs ", X)
+
+    Y = tf.placeholder(tf.float32, name="output")
+    tf.summary.histogram("outputs ", Y)
+
+    with tf.name_scope("Hidden_Layer"):
+        W1 = tf.Variable(tf.zeros([n_input, n_hidden]), name="W1")
+        tf.summary.histogram("Weights 1", W1)
+        b1 = tf.Variable(tf.zeros([n_hidden]), name="B1")
+        tf.summary.histogram("Biases 1", b1)
+        L2 = tf.nn.sigmoid(tf.matmul(X, W1) + b1)
+        tf.summary.histogram("Activation", L2)
+    with tf.name_scope("OutputLayer"):
+        W2 = tf.Variable(tf.zeros([n_hidden, n_output]), name="W2")
+        tf.summary.histogram("Weights 2", W2)
+        b2 = tf.Variable(tf.zeros([n_output]), name="B2")
+        tf.summary.histogram("Biases 2", b2)
+        hy = tf.nn.sigmoid(tf.matmul(L2, W2) + b2)
+        tf.summary.histogram("Output", hy)
+
+    # calculate the coast of our calculations and then optimaze it
+    with tf.name_scope("Coast"):
+        cost = tf.reduce_mean(-Y * tf.log(hy) - (1 - Y) * tf.log(1 - hy))
+        tf.summary.histogram("Cost ", cost)
+
+    with tf.name_scope("Train"):
+        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+        tf.summary.histogram("Optimazer ", optimizer.values())
+
+    with tf.name_scope("accuracy"):
+        answer = tf.equal(tf.floor(hy + 0.1), Y)
+        accuracy = tf.reduce_mean(tf.cast(answer, "float32"))
+        tf.summary.scalar("accuracy", accuracy)
+
+    summ = tf.summary.merge_all()
+    saver = tf.train.Saver()
+    """cost = tf.reduce_sum(tf.pow(y_ - y, 2)) / (2 * n_samples)
+      optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+     """
+    # Initialize variabls and tensorflow session
+    sess.run(tf.global_variables_initializer())
+    writer = tf.summary.FileWriter(LOGDIR)
+    writer.add_graph(sess.graph)
+
+    # lets Do  Our real traing
+
+    for i in range(training_epochs):
+        sess.run(optimizer, feed_dict={X: inputX, Y: inputY})
+        # Take a gradient descent step using our inputs and  labels
+
+        # That's all! The rest of the cell just outputs debug messages.
+        # Display logs per epoch step
+
+        if (i) % display_step == 0:
+            cc = sess.run(cost, feed_dict={X: inputX, Y: inputY})
+            print("Training step:", '%04d' % (i), "cost=", "{:.35f}".format(cc))
+            # print("\n  W1=", sess.run(W1), " \n W1=", sess.run(W2),
+            # "\n b1=", sess.run(b1), "b2=", sess.run(b2) )
+
+    print("\n ------------------------------------Optimization "
+          "Finished!------------------------------------------\n")
+    training_cost = cc
+    print("Training cost=", training_cost,
+          "\n W1 = \n", sess.run(W1), "\n W2= \n", sess.run(W2),
+          "\n b1=", sess.run(b1), '\n', "\n b2=", sess.run(b2), '\n')
+
+    answer = tf.equal(tf.floor(hy + 0.1), Y)
+    accuracy = tf.reduce_mean(tf.cast(answer, "float32"))
+    # print(sess.run([hy], feed_dict={X: inputX, Y: inputY}))
+    print("Accuracy: ", accuracy.eval({X: inputX, Y: inputY}, session=sess) * 100, "%")
+    print("final Coast = ", training_cost)
+    print("Parameters  :", "\n learning rate  = ", learning_rate, "\n epoches = ", training_epochs,
+          " \n hidden layers  = ", n_hidden, "\n coast function \n optimazer Adam ")
